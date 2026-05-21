ffddf6142e45509bb95aacd26e140ab7668e7f16
chittose <addinalbani14@gmail.com>
2026-05-22 02:36:14 +0700

    Update port for Railway

--- index.js
+++ index.js
@@ -42,8 +42,9 @@ setInterval(() => {
 
 app.get('/', (req, res) => res.send('Titik Nongkrong API is Online!'));
 app.get('/ping', (req, res) => res.send('pong'));
-app.listen(3000, () => {
-    console.log('✅ Web API Server ready.');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`✅ Web API Server ready on port ${port}.`);
     const SELF_URL = process.env.RENDER_EXTERNAL_URL
         ? `${process.env.RENDER_EXTERNAL_URL}/ping`
         : process.env.REPLIT_DOMAINS
